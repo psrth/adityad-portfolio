cf1279bac48c475ba72145dd303037415109b7cb
Parth Sharma <parthsharma151@gmail.com>
2020-09-14 11:20:55 +0530

    init

---
+++ build/precache-manifest.28b8c695f408e68c45b81f96fb814707.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "729636bd4473749ba29dd064faadb53f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac6c9179058386093c89",
+    "url": "/static/css/main.fa523a57.chunk.css"
+  },
+  {
+    "revision": "7ce781d35c6e17377a18",
+    "url": "/static/js/2.c2c21645.chunk.js"
+  },
+  {
+    "revision": "aa2a0ac3768dbda00ac12932e575a539",
+    "url": "/static/js/2.c2c21645.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac6c9179058386093c89",
+    "url": "/static/js/main.701409dc.chunk.js"
+  },
+  {
+    "revision": "a551bc9c28b1c60ac17f",
+    "url": "/static/js/runtime-main.df8cc1a8.js"
+  },
+  {
+    "revision": "fa5e3f2904989951119a4332cf9acd69",
+    "url": "/static/media/angad.fa5e3f29.jpeg"
+  }
+]);
